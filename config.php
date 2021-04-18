e6161f0916f179a54dfdcc4399bc1549ad4d5930
jmmds <31960542+jmmds@users.noreply.github.com>
2021-04-18 17:04:47 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 define('PSM_DB_HOST', 'localhost');
 define('PSM_DB_PORT', '');
-define('PSM_DB_NAME', 'xpto_monitor');
-define('PSM_DB_USER', 'xpto_monitor');
-define('PSM_DB_PASS', 'sh3C03iFT4');
+define('PSM_DB_NAME', '');
+define('PSM_DB_USER', '');
+define('PSM_DB_PASS', '');
 define('PSM_DB_PREFIX', 'psm_');
-define('PSM_BASE_URL', 'http://xpto.myhubdns.us');
+define('PSM_BASE_URL', '');
